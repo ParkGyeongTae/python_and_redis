79c416a2545fe77ef6c92c6520766ccb737baa11
박경태 <pgt0409@gmail.com>
2022-11-25 15:47:24 +0900

    key value 변경

--- code/3_set_get_redis.py
+++ code/3_set_get_redis.py
@@ -25,10 +25,16 @@ redis_host, redis_port, redis_password = get_redis_info()
 
 redis_client = redis.StrictRedis(host = redis_host, port = redis_port, db = 0, password = redis_password)
 
-redis_client.set('1', '2')
-redis_client.set('3', '4')
+redis_client.set('key_1', 'value_1')
+redis_client.set('key_2', 'value_2')
+redis_client.set('key_3', 'value_3')
+redis_client.set('key_4', 'value_4')
+redis_client.set('key_5', 'value_5')
 
-print(redis_client.get('1'))
-print(redis_client.get('3'))
+print(redis_client.get('key_1'))
+print(redis_client.get('key_2'))
+print(redis_client.get('key_3'))
+print(redis_client.get('key_4'))
+print(redis_client.get('key_5'))
 
 # redis_client.flushall()
