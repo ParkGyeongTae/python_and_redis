5f1e90347c694f4a4e412731f306df16e8b42374
박경태 <pgt0409@gmail.com>
2022-11-25 15:37:20 +0900

    레디스 비밀번호 환경변수 파일 분리

--- code/3_set_get_redis.py
+++ code/3_set_get_redis.py
@@ -1,20 +1,29 @@
 import os
 import redis
 
-if os.environ.get('redis_host') == None:
-    os.environ['redis_host'] = 'redis'
-if os.environ.get('redis_port') == None:
-    os.environ['redis_port'] = '6379'
+def get_redis_info():
 
-env_dict = {}
+    if os.environ.get('REDIS_HOST') == None:
+        os.environ['REDIS_HOST'] = 'redis'
+    if os.environ.get('REDIS_PORT') == None:
+        os.environ['REDIS_PORT'] = '6379'
+    if os.environ.get('REDIS_PASSWORD') == None:
+        os.environ['REDIS_PASSWORD'] = 'password'
 
-for key, value in os.environ.items():
-    env_dict.setdefault(key, value)
+    env_dict = {}
 
-redis_host = env_dict['redis_host']
-redis_port = env_dict['redis_port']
+    for key, value in os.environ.items():
+        env_dict.setdefault(key, value)
 
-redis_client = redis.StrictRedis(host = redis_host, port = redis_port, db = 0, password = 'password')
+    redis_host = env_dict['REDIS_HOST']
+    redis_port = env_dict['REDIS_PORT']
+    redis_password = env_dict['REDIS_PASSWORD']
+
+    return redis_host, redis_port, redis_password
+
+redis_host, redis_port, redis_password = get_redis_info()
+
+redis_client = redis.StrictRedis(host = redis_host, port = redis_port, db = 0, password = redis_password)
 
 redis_client.set('1', '2')
 redis_client.set('3', '4')
