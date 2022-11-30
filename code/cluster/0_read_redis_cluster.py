c61d70a28a92ffc11ef73448a78e8c8e87f94f60
박경태 <pgt0409@gmail.com>
2022-11-30 20:42:02 +0900

    correct 0_read_redis_cluster

--- code/cluster/0_read_redis_cluster.py
+++ code/cluster/0_read_redis_cluster.py
@@ -1,50 +1,45 @@
 from rediscluster import RedisCluster
 import os
 
-class RedisSetting():
+class RedisClusterSetting():
 
-    def get_redis_info():
+    def get_redis_cluster_info():
 
-        if os.environ.get('REDIS_HOST') == None:
-            os.environ['REDIS_HOST'] = 'redis_1'
-        if os.environ.get('REDIS_PORT') == None:
-            os.environ['REDIS_PORT'] = '6379'
-        if os.environ.get('REDIS_PASSWORD') == None:
-            os.environ['REDIS_PASSWORD'] = 'password'
+        if os.environ.get('REDIS_CLUSTER_HOST') == None:
+            os.environ['REDIS_CLUSTER_HOST'] = 'redis_1,redis_2,redis_3'
+        if os.environ.get('REDIS_CLUSTER_PORT') == None:
+            os.environ['REDIS_CLUSTER_PORT'] = '6379,6379,6379'
+        if os.environ.get('REDIS_CLUSTER_PASSWORD') == None:
+            os.environ['REDIS_CLUSTER_PASSWORD'] = 'password'
 
         env_dict = {}
 
         for key, value in os.environ.items():
             env_dict.setdefault(key, value)
 
-        redis_host = env_dict['REDIS_HOST']
-        redis_port = env_dict['REDIS_PORT']
-        redis_password = env_dict['REDIS_PASSWORD']
+        redis_cluster_host = env_dict['REDIS_CLUSTER_HOST'].split(',')
+        redis_cluster_port = env_dict['REDIS_CLUSTER_PORT'].split(',')
+        redis_cluster_password = env_dict['REDIS_CLUSTER_PASSWORD']
 
-        return redis_host, redis_port, redis_password
+        return redis_cluster_host, redis_cluster_port, redis_cluster_password
 
 if __name__ == '__main__':
 
-    redis_host, redis_port, redis_password = RedisSetting.get_redis_info()
+    redis_cluster_host, redis_cluster_port, redis_cluster_password = RedisClusterSetting.get_redis_cluster_info()
 
-    redis_client = RedisCluster(
-        startup_nodes = [dict(host = redis_host, port = redis_port)],
-        password = redis_password,
-        decode_responses = True, 
-        skip_full_coverage_check = True)
-    
-    print(redis_client)
-    exit()
+    for i in range(len(redis_cluster_host)):
+        redis_host = redis_cluster_host[i]
+        redis_port = redis_cluster_port[i]
 
-    redis_client.set(1, 3)
-    redis_client.expire(1, 5)
-    print(redis_client.get(1))
+        try:
+            redis_client = RedisCluster(
+                startup_nodes = [dict(host = redis_host, port = redis_port)],
+                password = redis_cluster_password,
+                decode_responses = True, 
+                skip_full_coverage_check = True)
+        except:
+            print('Do not connected', redis_host)
 
-
-# redis.StrictRedis(host = redis_host, port = redis_port, db = 0, password = redis_password)
-
-
-
-# client = RedisCluster(
-# startup_nodes=[dict(host=os.environ.get('REDIS_HOST'), port=os.environ.get('REDIS_PORT'))],
-# decode_responses=True, skip_full_coverage_check=True)
+    redis_client.set(2, 2)
+    redis_client.expire(2, 5)
+    print(redis_client.get(2))
